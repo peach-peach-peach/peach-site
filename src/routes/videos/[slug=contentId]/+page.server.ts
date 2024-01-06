3209a1e5596467d55732eeac876c6e558d1ce9a5
peach-peach-peach <skgm0301+peach@gmail.com>
2024-01-06 20:23:06 +0900

    preview pages

--- src/routes/videos/[slug=contentId]/+page.server.ts
+++ src/routes/videos/[slug=contentId]/+page.server.ts
@@ -2,16 +2,27 @@ import type { PageServerLoad } from './$types'
 import type { Article } from '../../../domain/contents/Article'
 import { createMicroCMSClient } from '$lib/sdk/microcms'
 
-export const load: PageServerLoad = async ({ params }) => {
+export const load: PageServerLoad = async ({ params, url }) => {
+	const draftKey = url.searchParams.get('draftKey')
 	const client = createMicroCMSClient()
+
+	if (draftKey == null) {
+		const articleRes = await client.get<Article>({
+			endpoint: 'articles',
+			contentId: params.slug
+		})
+
+		return { item: articleRes, isPreview: false }
+	}
+
+	// プレビューモード
 	const articleRes = await client.get<Article>({
 		endpoint: 'articles',
-		contentId: params.slug
+		contentId: params.slug,
+		queries: { draftKey }
 	})
 
-	return {
-		item: articleRes
-	}
+	return { item: articleRes, isPreview: true }
 }
 
 export const ssr = false

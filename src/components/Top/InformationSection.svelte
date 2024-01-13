26ab2775ac647d8852d3735ea0028d8a42e5f8d9
peach-peach-peach <skgm0301+peach@gmail.com>
2024-01-13 22:13:03 +0900

    feat: info/videos

--- src/components/Top/InformationSection.svelte
+++ src/components/Top/InformationSection.svelte
@@ -10,38 +10,81 @@
 	export let data: Data
 </script>
 
-<section>
-	<h2>Information</h2>
+<section class="information">
+	<header>
+		<h2>Information</h2>
+	</header>
+
 	<section>
 		<h3>News</h3>
 		<ul>
 			{#each data.news as item}
 				<li>
 					<a href={`/news/${item.id}/`}>{item.title}</a>
-					<time>{formatDate(item.publishedAt)}</time>
 				</li>
 			{/each}
 		</ul>
 	</section>
+
 	<section>
 		<h3>Schedule</h3>
 		<ul>
 			{#each data.schedule as item}
 				<li>
 					<a href={`/schedule/${item.id}/`}>{item.title}</a>
-					<time>{formatDate(item.publishedAt)}</time>
 				</li>
 			{/each}
 		</ul>
 	</section>
 </section>
 
-<style>
-	section {
+<style lang="scss">
+	.information {
+		display: grid;
+		grid-template-columns: 3rem 1fr 1fr;
+
+		border: 1px solid #000;
+		min-height: 10rem;
+	}
+
+	header {
 		display: flex;
-		flex-direction: column;
-		justify-content: center;
 		align-items: center;
-		flex: 0.6;
+		justify-content: center;
+
+		border-right: 1px solid #000;
+		background-color: var(--color-key-green);
+	}
+
+	header > h2 {
+		rotate: 270deg;
+		margin: 0;
+		font-size: 1.1rem;
+		font-weight: normal;
+	}
+
+	.information {
+		section {
+			padding: 1rem;
+		}
+
+		section + section {
+			border-left: 1px solid #000;
+		}
+
+		h3 {
+			margin: 0 0 1rem 0;
+		}
+
+		ul {
+			list-style: none;
+			padding: 0;
+			margin: 0;
+		}
+
+		li {
+			line-height: 1.8;
+			font-size: 1.2rem;
+		}
 	}
 </style>
--- src/components/Top/VideoSection.svelte
+++ src/components/Top/VideoSection.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { formatDate } from '@/lib/utils/formatDate'
 	import type { Article } from '@/domain/contents/Article'
+	import Spacer from '@/components/Spacer.svelte'
 
 	type Data = {
 		videos: Article[]
@@ -14,19 +15,84 @@
 	<ul>
 		{#each data.videos as item}
 			<li>
-				<a href={`/videos/${item.id}/`}>{item.title}</a>
+				<a href={`/videos/${item.id}/`}><img src={item.eyecatch?.url} alt={item.title} /></a>
+				<a href={`/videos/${item.id}/`} class="title">{item.title}</a>
+				<!-- time分のスペースを確保 -->
+				<Spacer size="3rem" />
 				<time>{formatDate(item.publishedAt)}</time>
 			</li>
 		{/each}
 	</ul>
+	<p class="see-all"><a href="/videos">See All Videos →</a></p>
 </section>
 
-<style>
+<style lang="scss">
 	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
+		border: 1px solid #000;
+		border-top: none;
+	}
+
+	h2 {
+		margin: 0;
+		padding: 1rem;
+
+		background-color: var(--color-key-yellow);
+		border-bottom: 1px solid #000;
+		text-align: center;
+
+		font-weight: normal;
+		font-size: 1rem;
+	}
+
+	ul {
+		display: grid;
+		grid-template-columns: 1fr 1fr 1fr;
+		padding: 0;
+		margin: 0;
+	}
+
+	li {
+		display: block;
+		padding: 1rem;
+		position: relative;
+
+		& + & {
+			border-left: 1px solid #000;
+		}
+
+		img {
+			width: 100%;
+			height: auto;
+		}
+
+		.title {
+			display: block;
+			margin-top: 1rem;
+		}
+
+		time {
+			display: block;
+			font-size: 0.8rem;
+			position: absolute;
+			bottom: 1rem;
+		}
+	}
+
+	.see-all {
+		margin: 0;
+		padding: 0.5rem;
+
+		text-align: center;
+		background-color: var(--color-bg-black);
+
+		a {
+			color: #fff;
+			text-decoration: none;
+		}
+
+		a:hover {
+			background-color: var(--color-bg-black);
+			text-decoration: underline;
+		}
 	}
 </style>

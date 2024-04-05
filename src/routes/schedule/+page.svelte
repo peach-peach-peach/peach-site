2b5de7417383dcaf7f8bd50e6bf99fdf7c5eeb4c
peach-peach-peach <skgm0301+peach@gmail.com>
2024-04-05 22:18:46 +0900

    fix: fetch more test

--- src/routes/schedule/+page.svelte
+++ src/routes/schedule/+page.svelte
@@ -1,11 +1,24 @@
 <script lang="ts">
 	import type { PageData } from './$types'
 	import CardList from '@/components/CardList.svelte'
-	import SeeAll from '@/components/SeeAll.svelte'
 	import Footer from '@/components/Footer.svelte'
 	import MarqueeHeader from '@/components/MarqueeHeader.svelte'
+	import type { APIFetchMoreResponse } from '../api/fetch-more/+server'
 
 	export let data: PageData
+	$: contents = data.contents
+	$: hasMore = contents.length < data.totalCount
+
+	const handleClick = async () => {
+		if (hasMore) {
+			const params = new URLSearchParams({ categoryId: 'schedule', limit: `${1}`, offset: `${data.contents.length}` })
+			const nextPageRes: APIFetchMoreResponse = await fetch(`/api/fetch-more?${params.toString()}`, {
+				method: 'GET',
+				headers: { 'Content-Type': 'application/json' }
+			}).then(res => res.json())
+			contents = [...contents, ...nextPageRes.contents]
+		}
+	}
 </script>
 
 <svelte:head>
@@ -17,12 +30,32 @@
 <MarqueeHeader contentType="schedule" />
 
 <section>
-	<CardList categoryId="schedule" items={data.contents} --theme-color="var(--color-key-blue)" />
+	<CardList categoryId="schedule" items={contents} --theme-color="var(--color-key-blue)" />
 </section>
 
-<SeeAll href="/schedule/N">More</SeeAll>
+{#if hasMore}
+	<div class="see-all"><a on:click={handleClick} href="#top">More</a></div>
+{/if}
 
 <Footer />
 
-<style>
+<style lang="scss">
+	.see-all {
+		margin: 0;
+		padding: 0.5rem;
+
+		text-align: center;
+		font-family: 'Montserrat', sans-serif;
+		background-color: var(--color-bg-dark);
+
+		a {
+			color: var(--color-text-highlight);
+			text-decoration: none;
+		}
+
+		a:hover {
+			background-color: transparent;
+			text-decoration: underline;
+		}
+	}
 </style>

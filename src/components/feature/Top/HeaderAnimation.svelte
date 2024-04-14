e2c1e146fc1791d82816620a64a5787d8aacd318
peach-peach-peach <skgm0301+peach@gmail.com>
2024-04-14 17:11:04 +0900

    fix: separate catch

---
+++ src/components/feature/Top/HeaderAnimation.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { onMount } from 'svelte'
+	import { createScene1, subscribeResize, subscribeScroll } from '$lib/three/scene1'
+
+	let el: HTMLCanvasElement
+
+	onMount(() => {
+		const unsubscribeResize = subscribeResize()
+		const unsubscribeScroll = subscribeScroll()
+		createScene1(el)
+
+		return () => {
+			unsubscribeResize()
+			unsubscribeScroll()
+		}
+	})
+</script>
+
+<div class="catch">
+	<canvas bind:this={el} />
+</div>
+
+<style>
+	.catch {
+		width: 100%;
+		height: 100vh;
+	}
+</style>

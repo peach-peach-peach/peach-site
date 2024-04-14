1d1ee40f9f89848aa39139afbca976b0fd743cc3
peach-peach-peach <skgm0301+peach@gmail.com>
2024-04-14 17:24:19 +0900

    fix: profile design

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -17,23 +17,25 @@
 <article>
 	<h1>Profile</h1>
 
-	<img src="/ppp.jpg" alt="Profile" class="eyecatch" />
+	<img src="/ppp.jpg" alt="Peach Peach Peachのアーティスト画像" class="eyecatch" />
+
+	<h2>プロフィール / Profile</h2>
 
 	<p>
-		○2021年秋、東京で活動を開始。<br />
+		2021年秋、東京で活動を開始。<br />
 		from福岡の昔からの友だち2人が 一緒に遊んでた果てに、音楽をつくる事に。<br />
 		hiphopもindie rockもneo soulも、好きなジャンルを自在にオーライ。 <br />
 		とにかく楽しみたくて音楽をやってる。
 	</p>
 
-	<p>
-		○2021, autumn. Started in Tokyo.<br />
+	<p lang="en">
+		2021, autumn. Started in Tokyo.<br />
 		2 old friends from Fukuoka, hanging out and playing, end up making songs together.<br />
 		From hiphop to indie rock, freely open genre is welcome to us.<br />
 		We just wanna play music ‘cause we want to have fun!!!
 	</p>
 
-	<h2>▲メンバー</h2>
+	<h2>メンバー / Member</h2>
 	<p>
 		川関浩司:<br />
 		1999年福岡でオルタナティブバンドNANANINEを結成。2001年ワーナーミュージックよりメジャーデビュー、海外フェスや国内大型フェスに出演。2006年バンド活動休止後はマイペースに音楽に関わり続けている。
@@ -43,13 +45,12 @@
 		2004年、ソロプロジェクトALOHAとして福岡で始動。その後東京に活動拠点を移し、2011年に曽我部恵一主宰のROSE RECORDSより『うたのゆくえ』をリリース。自身の楽曲制作の他に、プロデュースやマスタリングなども手掛ける。
 	</p>
 
-	<h2>▲Member</h2>
-	<p>
+	<p lang="en">
 		Hiroshi Kawasaki:<br />
 		In 1999, formed Alternative Band “NANANINE” in Fukuoka. In 2001, major debut in Warner Music. Performed in international/domestic
 		music festivals.
 	</p>
-	<p>
+	<p lang="en">
 		Daisuke Nishio:<br />
 		In 2004, start Solo project as ALOHA in Fukuoka. Afterwards, moved the base to Tokyo. In addition to producing my own
 		music, I also produce other peoples producing and mastering.
@@ -70,8 +71,31 @@
 	}
 
 	h1 {
+		font-weight: bold;
 		font-size: 1.4rem;
-		margin: 0 0 1rem;
+		margin: 0 0 2rem;
+	}
+
+	.eyecatch {
+		width: 100%;
+		height: auto;
+		margin: 4rem 0;
+		box-shadow: 0 4px 16px rgba(0, 0, 0, 0.3);
+	}
+
+	h2 {
+		font-weight: bold;
+		font-size: 1.2rem;
+		margin: 3rem 0 1rem;
+	}
+
+	h2::before {
+		content: '▲';
+		margin-right: 0.2rem;
+	}
+
+	p {
+		line-height: 2.5;
 	}
 
 	.back {

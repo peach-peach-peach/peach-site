603be594fb80b16af6fdf1be5af63a429d27fe37
peach-peach-peach <skgm0301+peach@gmail.com>
2024-04-29 14:58:42 +0900

    fix: forma

--- tests/e2e/root.test.ts
+++ tests/e2e/root.test.ts
@@ -7,9 +7,9 @@ test('topページにロゴが表示されている', async ({ page }) => {
 
 test('ナビゲーションが表示されている', async ({ page }) => {
 	await page.goto('/')
-	await expect(page.getByRole('link', { name: 'News', exact: true  })).toBeVisible()
-	await expect(page.getByRole('link', { name: 'Schedule', exact: true  })).toBeVisible()
-	await expect(page.getByRole('link', { name: 'Videos', exact: true  })).toBeVisible()
+	await expect(page.getByRole('link', { name: 'News', exact: true })).toBeVisible()
+	await expect(page.getByRole('link', { name: 'Schedule', exact: true })).toBeVisible()
+	await expect(page.getByRole('link', { name: 'Videos', exact: true })).toBeVisible()
 	await expect(page.getByRole('link', { name: 'Discography', exact: true })).toBeVisible()
-	await expect(page.getByRole('link', { name: 'Profile', exact: true  })).toBeVisible()
+	await expect(page.getByRole('link', { name: 'Profile', exact: true })).toBeVisible()
 })

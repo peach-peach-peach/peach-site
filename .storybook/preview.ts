ba5873ff9e560f72f5434fb3278e46986226b60e
peach-peach-peach <skgm0301+peach@gmail.com>
2024-05-05 12:33:42 +0900

    fix: sb: CardList

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,8 @@
 import type { Preview } from '@storybook/svelte'
 
+import '../src/routes/reset.css'
+import '../src/routes/styles.css'
+
 const preview: Preview = {
 	parameters: {
 		controls: {
@@ -7,6 +10,14 @@ const preview: Preview = {
 				color: /(background|color)$/i,
 				date: /Date$/i
 			}
+		},
+		backgrounds: {
+			values: [
+				{
+					name: 'pink',
+					value: '#ffe3e3'
+				}
+			]
 		}
 	}
 }
---
+++ src/components/feature/CardList.stories.ts
@@ -0,0 +1,46 @@
+import type { Meta, StoryObj } from '@storybook/svelte'
+import CardList from './CardList.svelte'
+
+const meta = {
+	title: 'feature/CardList',
+	component: CardList,
+	tags: ['autodocs'],
+	parameters: {
+		layout: 'fullscreen'
+	}
+} satisfies Meta<CardList>
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+const getItems = (count: number) => {
+	return Array.from({ length: count }, (_, i) => ({
+		id: `${i + 1}`,
+		title: `Card Title ${i + 1}`,
+		eyecatch: {
+			url: 'https://placehold.jp/150x150.png'
+		},
+		publishedAt: '2021-01-01T00:00:00+09:00'
+	}))
+}
+
+export const CardX1: Story = {
+	args: {
+		categoryId: 'news',
+		items: getItems(1)
+	}
+}
+
+export const CardX3: Story = {
+	args: {
+		categoryId: 'news',
+		items: getItems(3)
+	}
+}
+
+export const CardX4: Story = {
+	args: {
+		categoryId: 'news',
+		items: getItems(4)
+	}
+}

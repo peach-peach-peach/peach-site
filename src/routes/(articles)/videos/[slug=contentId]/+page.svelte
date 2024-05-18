5436a254157fc83758d3eedf69d532b6cb76d1f8
peach-peach-peach <skgm0301+peach@gmail.com>
2024-05-18 13:20:02 +0900

    fix: メタタグ設定 2

--- src/routes/(articles)/videos/[slug=contentId]/+page.svelte
+++ src/routes/(articles)/videos/[slug=contentId]/+page.svelte
@@ -1,13 +1,8 @@
 <script lang="ts">
-	import ArticlePage from '@/components/feature/ArticlePage.svelte'
 	import type { PageData } from './$types'
-	import { site } from '@/constants/site'
+	import ArticlePage from '@/components/feature/ArticlePage.svelte'
 
 	export let data: PageData
 </script>
-
-<svelte:head>
-	<title>Video: {data.item.title} | {site.title}</title>
-</svelte:head>
 
 <ArticlePage category="videos" {data} hideCatch />

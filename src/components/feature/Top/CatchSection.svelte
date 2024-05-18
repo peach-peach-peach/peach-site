91ececf0aca9562895990f25c6b46761854018fd
peach-peach-peach <skgm0301+peach@gmail.com>
2024-05-18 12:15:39 +0900

    fix: replace sp video

--- src/components/feature/Top/CatchSection.svelte
+++ src/components/feature/Top/CatchSection.svelte
@@ -1,7 +1,25 @@
+<script>
+	import MediaQuery from '@/components/primitive/MediaQuery.svelte'
+</script>
+
 <aside>
-	<video src="/videos/easy-muted.mp4" poster="/videos/easy-muted-thumb.jpg" autoplay muted loop playsinline>
-		<track kind="captions" />
-	</video>
+	<MediaQuery query="all and (min-width: 768px)" let:matches>
+		{#if matches}
+			<video poster="/videos/easy-thumb.jpg" autoplay muted loop playsinline>
+				<source src="/videos/easy.mp4" type="video/mp4" />
+				<track kind="captions" />
+			</video>
+		{/if}
+	</MediaQuery>
+	<MediaQuery query="all and (max-width: 767px)" let:matches>
+		{#if matches}
+			<video poster="/videos/easy_sp-thumb.jpg" autoplay muted loop playsinline>
+				<source src="/videos/easy_sp.webm" type="video/webm" />
+				<source src="/videos/easy_sp.mp4" type="video/mp4" />
+				<track kind="captions" />
+			</video>
+		{/if}
+	</MediaQuery>
 </aside>
 
 <style>
